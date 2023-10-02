de7791c57522747d0e9fe1456cdd54e6c58e86ca
Ti-mo-n <timonnuwagaba@gmail.com>
2023-10-02 11:12:42 +0300

    Change from unfamiliar skills to familiar skills and storing data in db instead of txt file

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
+import sqlite3
 from bs4 import BeautifulSoup
 import requests
 import time
 
-print('Put skill that you are not familiar with')
-unfamiliar_skill = input('>')
-print(f'Filtering out {unfamiliar_skill}')
+print('Put skill that you are familiar with')
+familiar_skill = input('>')
+print(f'Filtering jobs that require {familiar_skill}')
+
+# Create a SQLite database and table
+conn = sqlite3.connect('jobs.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        company_name TEXT,
+        skills TEXT,
+        more_info TEXT
+    )
+''')
+conn.commit()
 
 def find_jobs():
-    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-    # print(html_text)
+    html_text = requests.get(f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={familiar_skill}&txtLocation=').text
     soup = BeautifulSoup(html_text, 'lxml')
-    # jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-    # print(jobs)
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-    # print(job)
+
     for index, job in enumerate(jobs):
         published_date = job.find('span', class_='sim-posted').span.text
         if 'few' in published_date:
-            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            company_name = job.find('h3', class_='joblist-comp-name').text.strip()
+            skills = job.find('span', class_='srp-skills').text.strip()
             more_info = job.header.h2.a['href']
-            if unfamiliar_skill not in skills:
-                with open(f'Post/{index}.txt', 'w') as f:
-                    f.write(f"Company Name: {company_name.strip()} \n")
-                    f.write(f"Required Skills: {skills.strip()} \n")
-                    f.write(f"More Info: {more_info} \n")
-                print(f'File saved: {index}')
-
+            if familiar_skill in skills:
+                cursor.execute("INSERT INTO jobs (company_name, skills, more_info) VALUES (?, ?, ?)",
+                               (company_name, skills, more_info))
+                conn.commit()
+                print(f'Job data inserted into database: {index}')
 
 if __name__ == '__main__':
     while True:
         find_jobs()
         time_wait = 10
-        print(f'waiting {time_wait} minutes...')
+        print(f'Waiting {time_wait} minutes...')
         time.sleep(time_wait * 60)
+
+# Close the database connection when done
+conn.close()
